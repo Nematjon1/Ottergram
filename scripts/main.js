baf5e22fddf7616ffdf1b5057369761df2692e5c
Nematjon <frank.sport.93@gmail.com>
2019-05-18 10:42:55 +0300

    styles updated

--- scripts/main.js
+++ scripts/main.js
@@ -10,39 +10,43 @@ ESC_KEY = 27;
 
 
 
-function setDetails(imageUrl, titleText){
+const setDetails = (imageUrl, titleText) => {
    let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
    detailImage.setAttribute('src', imageUrl);
    let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
    detailTitle.textContent = titleText;
-}
-function imageFromThumb(thumbnail){
+};
+
+const imageFromThumb = (thumbnail) => {
    return thumbnail.getAttribute('data-image-url');
-}
-function titleFromThumb(thumbnail){
-   return thumbnail.getAttribute('data-image-title');
-}
-function setDetailsFromThumb(thumbnail){
+};
+
+const titleFromThumb = (thumbnail) => {
+  return thumbnail.getAttribute('data-image-title');
+;}
+
+const setDetailsFromThumb = (thumbnail) => {
    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 };
-function addThumbClickHendler(thumb){
+
+const addThumbClickHendler = (thumb) => {
    thumb.addEventListener('click', function(event){
       event.preventDefault();
       setDetailsFromThumb(thumb);
       showDetails();
    });
 };
-function getThumbnailsArray(){
+const getThumbnailsArray = () => {
    let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
    let thumbnailArray=[].slice.call(thumbnails);
    return thumbnailArray;
 };
 
-function hideDetails(){
+const hideDetails = () => {
    document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
-function showDetails(){
+const showDetails = () => {
    let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
    frame.classList.add(TINY_EFFECT_CLASS);
@@ -52,7 +56,7 @@ function showDetails(){
 };
 
 
-function addKeyPressHandler(){
+const addKeyPressHandler = () => {
    document.body.addEventListener('keyup', function(event){
       event.preventDefault();
       if(event.keyCode === ESC_KEY){
@@ -61,7 +65,7 @@ function addKeyPressHandler(){
    });
 };
 
-function initializeEvents(){
+const initializeEvents = () => {
    let thumbnails = getThumbnailsArray();
    thumbnails.forEach(addThumbClickHendler);
    addKeyPressHandler();
