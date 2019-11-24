2b0b635b7d5dd9bb4b154f41cc08156a42c3a373
Nematjon <frank.sport.93@gmail.com>
2019-11-24 10:30:28 +0300

    replaced let with const

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,58 @@
 ;'use strict';
-let DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]',
-DETAIL_TITLE_SELECTOR = '[data-image-role="title"]',
-DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]',
-THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]',
-HIDDEN_DETAIL_CLASS = 'hidden-detail',
-TINY_EFFECT_CLASS = 'is-tiny',
-ESC_KEY = 27;
+
+const DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+const DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+const DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+const THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+const HIDDEN_DETAIL_CLASS = 'hidden-detail';
+const TINY_EFFECT_CLASS = 'is-tiny';
+const ESC_KEY = 27;
 
 
 
 
 const setDetails = (imageUrl, titleText) => {
-   let detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+   const detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
    detailImage.setAttribute('src', imageUrl);
-   let detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+   const detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
    detailTitle.textContent = titleText;
 };
 
-const imageFromThumb = (thumbnail) => {
+const imageFromThumb = thumbnail => {
    return thumbnail.getAttribute('data-image-url');
 };
 
-const titleFromThumb = (thumbnail) => {
+const titleFromThumb = thumbnail => {
   return thumbnail.getAttribute('data-image-title');
 ;}
 
-const setDetailsFromThumb = (thumbnail) => {
+const setDetailsFromThumb = thumbnail => {
    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 };
 
-const addThumbClickHendler = (thumb) => {
+const addThumbClickHendler = thumb => {
    thumb.addEventListener('click', function(event){
       event.preventDefault();
       setDetailsFromThumb(thumb);
       showDetails();
    });
 };
+
 const getThumbnailsArray = () => {
-   let thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
-   let thumbnailArray=[].slice.call(thumbnails);
+   const thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+   const thumbnailArray = [].slice.call(thumbnails);
    return thumbnailArray;
 };
 
 const hideDetails = () => {
    document.body.classList.add(HIDDEN_DETAIL_CLASS);
-}
+};
 
 const showDetails = () => {
-   let frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+   const frame = document.querySelector(DETAIL_FRAME_SELECTOR);
    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
    frame.classList.add(TINY_EFFECT_CLASS);
-   setTimeout(function(){
+   setTimeout(function() {
       frame.classList.remove(TINY_EFFECT_CLASS);
    }, 140);
 };
@@ -66,7 +68,7 @@ const addKeyPressHandler = () => {
 };
 
 const initializeEvents = () => {
-   let thumbnails = getThumbnailsArray();
+   const thumbnails = getThumbnailsArray();
    thumbnails.forEach(addThumbClickHendler);
    addKeyPressHandler();
 };
